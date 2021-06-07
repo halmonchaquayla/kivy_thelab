5c67dc13be07a01c7373f1886975a68c900f9086
Chaquayla Halmon <halmonchaquayla@gmail.com>
2021-06-06 22:22:15 -0400

    Learning gridlayoutexample

--- venv/main.py
+++ venv/main.py
@@ -1,9 +1,13 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
+class GridLayoutExample(GridLayout): #g1
+    #organize content with columns and rows 
+    pass
 class AnchorLayoutExample(AnchorLayout):
     pass
 
